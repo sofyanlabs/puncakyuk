8834258e256b1f528122b7f6913d0d63c8e15637
Sofyanlabs <a.sofyann27@gmail.com>
2019-12-16 02:34:38 +0700

    first commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="antialiased bg-gray-100">
+    <header class="container flex flex-wrap items-center py-4">
+      <div class="flex-1 flex justify-between items-center text-center">
+        <a href="#" class="font-bold text-2xl text-teal-500">
+          <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+        </a>
+      </div>
+
+      <div class="flex items-center w-auto" id="menu">
+        <nav>
+          <ul class="flex items-center justify-between text-base text-gray-700 lg:pt-0">
+            <li><a class="lg:p-4 px-0 hidden md:block border-b-2 border-transparent hover:border-teal-500 mr-5"
+                href="#">List Villa</a></li>
+            <li><a class="bg-teal-500 hover:bg-teal-600 text-white font-semibold uppercase py-2 px-4 rounded-lg tracking-wider"
+                href="#">Pesan Cepat</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <slot />
+  </div>
+</template>
+
+<static-query>
+  query {
+  metadata {
+  siteName
+  }
+  }
+</static-query>
---
+++ src/pages/Index.vue
@@ -0,0 +1,112 @@
+<template>
+  <Layout>
+    <div class="container mt-12 text-center">
+      <span class="font-bold uppercase tracking-wider text-xs md:text-sm text-gray-700">Liburan Lebih Tenang
+        Dengan</span>
+      <h1 class="font-bold text-2xl sm:text-3xl lg:text-4xl py-3 text-teal-500">Sewa Villa di Puncak Menjadi Lebih Mudah
+      </h1>
+      <p class="hidden sm:block sm:px-20 lg:px-0 text-xl text-gray-600">Kami memiliki pilihan <strong>Rekomendasi
+          Villa</strong> terbaik yang <strong>dapat kamu sewa</strong> dengan mudah!</p>
+      <img src="img/header.svg" alt="Gambar Cari Villa di Puncakyuk" class="mx-auto py-4 md:max-w-2xl xl:max-w-3xl" />
+    </div>
+
+    <div class="container">
+      <div
+        class="py-2 px-6 xl:py-6 w-full lg:w-5/6 xl:w-2/3 mx-auto md:flex justify-between items-center bg-white shadow-lg rounded border border-gray-200">
+        <h2 class="font-semibold text-base md:text-lg text-center text-gray-700">Mau Pesan Untuk Kapan Nih?</h2>
+        <div class="flex flex-wrap justify-center py-4 xl:py-0">
+          <date-picker v-model="time" range format="DD-MMMM-YYYY" placeholder="Pilih Tanggal..."
+            :disabled-date="notBeforeToday"></date-picker>
+        </div>
+        <div class="text-center">
+          <a class="bg-teal-500 hover:bg-teal-600 text-white text-sm font-semibold uppercase py-2 px-4 rounded-lg tracking-wider"
+            href="#">Pesan</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="container mt-20">
+      <div class="flex flex-wrap items-center">
+        <div class="w-full order-last md:order-first md:w-1/2">
+          <h2 class="text-teal-500 text-xl lg:text-3xl xl:text-4xl font-bold">
+            Menginap Nyaman Tanpa Pusing
+          </h2>
+          <p class="text-gray-600 text-sm lg:text-normal xl:text-lg mt-4 pr-0 lg:pr-20 sm:pl-0">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor rerum, tenetur recusandae quas assumenda
+            possimus!
+          </p>
+          <div class="flex items-start mt-8">
+            <div class="bg-teal-500 p-2 mr-4 mb-4 md:mb-0 rounded-lg w-12">
+              <img src="img/favorites.svg" alt="Harga Murah" class="max-w-sm h-8">
+            </div>
+            <div class="-mt-2 md:mt-0">
+              <h3 class="font-semibold">Pelayanan Terbaik</h3>
+              <p class="text-gray-600 text-sm lg:text-normal md:pr-4">Pesan mudah dan nyaman dengan dibantu oleh admin
+                kami.</p>
+            </div>
+          </div>
+          <div class="flex items-start text-left mt-8">
+            <div class="bg-teal-500 p-2 mr-4 mb-4 md:mb-0 rounded-lg w-12">
+              <img src="img/home.svg" alt="Pilihan Villa" class="max-w-sm h-8">
+            </div>
+            <div class="-mt-2 md:mt-0">
+              <h3 class="font-semibold">Villa Terlengkap</h3>
+              <p class="text-gray-600 text-sm lg:text-normal md:pr-4">Banyaknya pilihan rekomendasi villa yang dapat
+                dipesan.</p>
+            </div>
+          </div>
+          <div class="flex items-start text-left mt-8">
+            <div class="bg-teal-500 p-2 mr-4 mb-4 md:mb-0 rounded-lg w-12">
+              <img src="img/money.svg" alt="Harga Murah" class="max-w-sm h-8">
+            </div>
+            <div class="-mt-2 md:mt-0">
+              <h3 class="font-semibold">Harga Termurah</h3>
+              <p class="text-gray-600 text-sm lg:text-normal md:pr-4">Dapatkan harga termurah sesuai fasilitas yang
+                didapat.</p>
+            </div>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 py-10">
+          <img src="img/cari-villa.svg" alt="Cari Villa Menjadi Mudah" class="mx-auto" />
+        </div>
+      </div>
+    </div>
+
+    <div style="background: url('/img/bg-pattern.svg'); position:relative" class="bg-pattern mt-20">
+      <div class="container py-20">
+        <h2 class="text-teal-500 text-xl lg:text-3xl xl:text-4xl font-bold">
+          Rekomendasi Villa Terbaik
+        </h2>
+        <p class="text-gray-600 text-sm lg:text-normal xl:text-lg">
+          Pilihan villa yang memenuhi kebutuhanmu
+        </p>
+      </div>
+    </div>
+
+  </Layout>
+</template>
+
+<script>
+  import DatePicker from 'vue2-datepicker';
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  export default {
+    components: {
+      DatePicker
+    },
+    data() {
+      return {
+        time: null,
+      };
+    },
+    methods: {
+      notBeforeToday(date) {
+        return date < today;
+      }
+    },
+    metaInfo: {
+      title: 'Sewa Villa di Puncak Murah dan Mudah'
+    }
+  }
+</script>
